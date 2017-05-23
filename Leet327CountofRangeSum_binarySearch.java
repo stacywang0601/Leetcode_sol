92008c46c01e29412a0d459add49db09ffce9d77
stacywang0601 <stacywang61@gmail.com>
2017-05-23 13:22:11 -0400

    327binarySearch

---
+++ Leet327CountofRangeSum_binarySearch.java
@@ -0,0 +1,65 @@
+public class Leet327CountofRangeSum_binarySearch{
+    
+   
+public  int countRangeSum(int[] nums, int lower, int upper) {
+		int n = nums.length;
+		ArrayList<Long> helper = new ArrayList<>();
+		long sum = 0;
+		helper.add(0, (long) 0);
+		int res = 0;
+		for (int i = 0; i < n; i++) {
+			sum += nums[i];
+			int left = less(helper, 0, helper.size() - 1, sum - upper);
+			int right = lessOrEqual(helper, 0, helper.size() - 1, sum - lower);
+			res += right - left;
+			int index = greater(helper, 0, helper.size() - 1, sum);
+			helper.add(index, sum);
+		}
+		return res;
+
+	}
+	/*find the max index that less than the target**/
+	private  int less(ArrayList<Long> helper, int l, int h, long k) {
+		while (l < h) {
+			int m = (l + h + 1) / 2;
+			if (helper.get(m) >= k) {
+				h = m - 1;
+			} else {
+				l = m;
+			}
+		}
+		if (helper.get(l) < k) {
+			return l;
+		}
+		return -1;
+	}
+    /*find the max index that less than or equal the target**/
+	private  int lessOrEqual(ArrayList<Long> helper, int l, int h, long k) {
+		while (l < h) {
+			int m = (l + h + 1) / 2;
+			if (helper.get(m) > k) {
+				h = m - 1;
+			} else {
+				l = m;
+			}
+		}
+		if (helper.get(l) <= k) {
+			return l;
+		}
+		return -1;
+	}
+	/*find the min index that greater than the target**/
+	private  int greater(ArrayList<Long> helper, int l, int h, long k) {
+		while (l <= h) {
+			int m = (l + h) / 2;
+			if (helper.get(m) <= k) {
+				l = m + 1;
+			} else {
+				h = m - 1;
+			}
+		}
+		return l;
+	}
+
+
+}
