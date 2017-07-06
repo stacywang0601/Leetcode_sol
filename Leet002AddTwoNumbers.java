df89b8c98a9197badf38fbb2cec0bccdb53be687
stacywang0601 <stacywang61@gmail.com>
2017-07-06 11:30:16 -0400

    update

--- Leet002AddTwoNumbers.java
+++ Leet002AddTwoNumbers.java
@@ -1,4 +1,4 @@
-package B05_21;
+package B07_06;
 
 /**
  * You are given two non-empty linked lists representing two non-negative integers. The digits are
@@ -11,34 +11,45 @@ package B05_21;
  * Output: 7 -> 0 -> 8
  */
 class ListNode {
-		int      val;
-		ListNode next;
+	int	     val;
+	ListNode	next;
 
-		ListNode(int x) {
-				val = x;
-		}
+	ListNode(int x) {
+		val = x;
+	}
 }
 
 public class Leet002AddTwoNumbers {
-		/** when l1 or l2 is null, add 0 to sum */
-		public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-				ListNode pre = new ListNode(0);
-				ListNode head = pre;
-				int carry = 0;
+	/**
+	 * when l1 or l2 is null, add 0 to sum
+	 * (l1 == null ? 0 : l1.val)
+	 * l1 != null || l2 != null || carry != 0
+	 * if (l1 != null)
+	 * l1 = l1.next;
+	 * null keeps null
+	 * 
+	 * */
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		ListNode dummy = new ListNode(0);
+		ListNode pre = dummy;
+		int carry = 0;
 
-				while (l1 != null || l2 != null || carry != 0) {
-						ListNode cur = new ListNode(0);
-						int sum = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carry;
-						cur.val = sum % 10;
-						carry = sum >= 10 ? 1 : 0;
-						pre.next = cur;
-						pre = cur;
-						// check null
-						l1 = (l1 == null) ? l1 : l1.next;
-						l2 = (l2 == null) ? l2 : l2.next;
-				}
-				return head.next;
+		while (l1 != null || l2 != null || carry != 0) {
+			ListNode cur = new ListNode(0);
+			int sum = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carry;
+			cur.val = sum % 10;
+			carry = sum / 10;
+			pre.next = cur;
+			pre = cur;
+
+			if (l1 != null)
+				l1 = l1.next;
+			if (l2 != null)
+				l2 = l2.next;
 
 		}
+		return dummy.next;
+
+	}
 
 }
