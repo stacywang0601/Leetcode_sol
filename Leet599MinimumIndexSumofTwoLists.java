0f5eb1dd6cf9791ec153913e49f3231ed47068e4
stacywang0601 <stacywang61@gmail.com>
2017-08-01 08:40:44 -0400

    use hashmap

---
+++ Leet599MinimumIndexSumofTwoLists.java
@@ -0,0 +1,50 @@
+package B08_01;
+
+/**
+ * Suppose Andy and Doris want to choose a restaurant for dinner, and they both have a list of
+ * favorite restaurants represented by strings.
+ * 
+ * You need to help them find out their common interest with the least list index sum. If there is a
+ * choice tie between answers, output all of them with no order requirement. You could assume there
+ * always exists an answer.
+ * 
+ * Example 1:
+ * Input:
+ * ["Shogun", "Tapioca Express", "Burger King", "KFC"]
+ * ["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"]
+ * Output: ["Shogun"]
+ * Explanation: The only restaurant they both like is "Shogun".
+ * Example 2:
+ * Input:
+ * ["Shogun", "Tapioca Express", "Burger King", "KFC"]
+ * ["KFC", "Shogun", "Burger King"]
+ * Output: ["Shogun"]
+ * Explanation: The restaurant they both like and have the least index sum is "Shogun" with index
+ * sum 1 (0+1).
+ **/
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Leet599MinimumIndexSumofTwoLists {
+	/** use hashmap */
+	public String[] findRestaurant(String[] list1, String[] list2) {
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		for (int i = 0; i < list1.length; i++)
+			map.put(list1[i], i);
+		List<String> res = new ArrayList<>();
+		int min_sum = Integer.MAX_VALUE, sum;
+		for (int j = 0; j < list2.length && j <= min_sum; j++) {
+			if (map.containsKey(list2[j])) {
+				sum = j + map.get(list2[j]);
+				if (sum < min_sum) {
+					res.clear();
+					res.add(list2[j]);
+					min_sum = sum;
+				} else if (sum == min_sum)
+					res.add(list2[j]);
+			}
+		}
+		return res.toArray(new String[res.size()]);
+	}
+}
