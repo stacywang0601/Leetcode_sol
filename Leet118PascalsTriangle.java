892e08fefec165a0ca1a6f9ce5c135a3e84192aa
stacywang0601 <stacywang61@gmail.com>
2017-08-09 16:42:05 -0400

    add 1 at the beginning each row, update row

---
+++ Leet118PascalsTriangle.java
@@ -0,0 +1,23 @@
+package B08_09;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leet118PascalsTriangle {
+	/**
+	 * each row add 1 at the beginning,
+	 * then add row.get(j) + row.get(j + 1) to update jth number of row
+	 */
+	public List<List<Integer>> generate(int numRows) {
+		List<List<Integer>> allrows = new ArrayList<List<Integer>>();
+		ArrayList<Integer> row = new ArrayList<Integer>();
+		for (int i = 0; i < numRows; i++) {
+			row.add(0, 1);
+			for (int j = 1; j < row.size() - 1; j++)
+				row.set(j, row.get(j) + row.get(j + 1));
+			allrows.add(new ArrayList<Integer>(row));
+		}
+		return allrows;
+
+	}
+}
