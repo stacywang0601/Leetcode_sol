06729257ed567bd2cd39bde31b9ba59582030538
stacywang0601 <stacywang61@gmail.com>
2017-08-13 14:48:14 -0400

    i-1j-1, i-1j, ij-1

---
+++ Leet221MaximalSquare.java
@@ -0,0 +1,41 @@
+package B08_13;
+
+/***
+ * Given a 2D binary matrix filled with 0's and 1's, find the largest square containing only 1's and
+ * return its area.
+ * 
+ * For example, given the following matrix:
+ * 
+ * 1 0 1 0 0
+ * 1 0 1 1 1
+ * 1 1 1 1 1
+ * 1 0 0 1 0
+ * Return 4.
+ */
+public class Leet221MaximalSquare {
+	/**
+	 * Dynamic programming
+	 * dp(i, j) represents the length of the square
+	 * whose lower-right corner is located at (i, j)
+	 * dp(i, j) = min{ dp(i-1, j-1), dp(i-1, j), dp(i, j-1) }
+	 */
+	public int maximalSquare(char[][] a) {
+		if (a == null || a.length == 0 || a[0].length == 0)
+			return 0;
+
+		int max = 0, n = a.length, m = a[0].length;
+
+		int[][] dp = new int[n + 1][m + 1];
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= m; j++) {
+				if (a[i - 1][j - 1] == '1') {
+					dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+					max = Math.max(max, dp[i][j]);
+				}
+			}
+		}
+		// return the area
+		return max * max;
+	}
+}
