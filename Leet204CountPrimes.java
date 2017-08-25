5f614fd059c075afd664328239d862787ac038a5
stacywang0601 <stacywang61@gmail.com>
2017-08-25 13:18:32 -0400

    1 is not prime notPrime[]

---
+++ Leet204CountPrimes.java
@@ -0,0 +1,20 @@
+package B08_25;
+
+/** Count the number of prime numbers less than a non-negative number, n. */
+class Leet204CountPrimes {
+	/** 0,1 are not prime number */
+	public int countPrimes(int n) {
+		boolean[] notPrime = new boolean[n];
+		int count = 0;
+		for (int i = 2; i < n; i++) {
+			if (notPrime[i] == false) {
+				count++;
+				for (int j = 2; i * j < n; j++) {
+					notPrime[i * j] = true;
+				}
+			}
+		}
+
+		return count;
+	}
+}
